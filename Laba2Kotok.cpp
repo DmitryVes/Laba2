2b579ca234c602ce3a1e2a6813d35fc67978132c
DmitryVes <94383632+DmitryVes@users.noreply.github.com>
2025-10-18 16:27:57 +0300

    Add files via upload

---
+++ Laba2Kotok.cpp
@@ -0,0 +1,14 @@
+﻿#include "Laba2KotokH.h"
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    Node* head = nullptr;
+    Node* tail = nullptr;
+
+    FillFromFile(head, tail);
+    Show(head);
+    DeleteList(head, tail);
+
+    return 0;
+}
